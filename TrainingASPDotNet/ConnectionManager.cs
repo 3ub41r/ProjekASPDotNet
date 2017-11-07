e69af1df18da450ce9af61f14af3d1620410e35f
zubair <zubairabuhasan@gmail.com>
2017-11-07 16:15:44 +0800

    Buat kerja banyak

---
+++ TrainingASPDotNet/ConnectionManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TrainingASPDotNet
+{
+    public static class ConnectionManager
+    {
+        public static SqlConnection GetConnection()
+        {
+            var connection = ConfigurationManager.ConnectionStrings["Database"].ToString();
+            return new SqlConnection(connection);
+        }
+    }
+}
